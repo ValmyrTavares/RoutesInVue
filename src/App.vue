d20c67ccbbd11014835607a3a6acd7b3a69dc0c5
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-04-05 12:30:40 -0300

    Home estilizado com imagem

--- src/App.vue
+++ src/App.vue
@@ -54,5 +54,18 @@ export default {
   .v-enter-active{
     transition:all .5s
   }
+  img{
+    max-width:100%;
+  }
+  .conteudo{
+    display:grid;
+    grid-template-columns: 1fr minmax(200px, 400px);
+    grid-gap:30px;
+  }
+  @media screen and (max-width:400px){
+    .conteudo {
+      display: block;
+    }
+  }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,28 @@
         <pageLoading/>
     </div>
     <transition>
-    <div v-if="api">
-        <p>{{api}}</p>
-    </div>
+        <div class="conteudo" v-if="api">
+            <div>
+                <h1>Sobre a {{api.titulo}}</h1>
+                <p>{{api.descricao}}</p>
+                <router-link class="btn-cursos" tag="button" to="/cursos">Cursos</router-link>
+                <div>
+                    <h2>Avaliações</h2>
+                    <ul>
+                        <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                            <p>{{avaliacao.nome}}</p>
+                            <p>{{avaliacao.descricao}}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <img src="../assets/aprender.png" alt="Aprenda">
+        </div>
     </transition>
+   
+   
+
    
    
     
@@ -29,6 +47,19 @@ created(){
 </script>
 
 
-<style>
+<style scoped>
+.btn-cursos{
+    border:none;
+    background:#4B8;
+    border-radius:4px;
+    color:white;
+    cursor:pointer;
+    box-shadow: 4px 2px rgba(0,0,0,.1);
+    padding:15px 40px;
+    font-size:1rem;
+    margin-top:10px;
+    margin-bottom:40px;
+    font-family:"Avenir",Arial, Helvetica, sans-serif
 
+}
 </style>
